5491e97c29589d75d9855d6ef996cf35eb39dbb2
shailesh_334 <shaileshgurle334@gmail.com>
2026-09-16 19:10:56 +0530

    made navbar responsive added hamburger button

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ return (
   <Navbar/>
   <Hero />
   <Footer/>   
-
 </div>
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,39 @@
+import { useState } from "react"
+
 const Navbar = ()=>{
 
-
+    const [open , setOpen] = useState(false);
     return (
-
+    <>
     <div className="flex justify-between items-center bg-slate-800 text-white p-5 sticky z-10 top-0 ">
-        <div>
+        <div className="text-xl ">
             Logo
         </div>
-        <div className="flex gap-6">
+
+        {/* Desktop nav links */}
+        <div className="hidden sm:flex gap-6">
             <p>Home</p>
             <p>About</p>
             <p>Contact</p>
         </div>
+        
+        {/* Mobile Hamburger Button */}
+        <button className="sm:hidden text-white text-xl cursor-pointer" onClick={()=> setOpen(!open)}>
+           ☰
+        </button>
+        
 
     </div>
-
-
+    
+    {/* Mobile nav links */}
+    {open && 
+    <div className="sm:hidden flex flex-col items-center gap-3 p-3 bg-slate-800 text-white">
+            <p>Home</p>
+            <p>About</p>
+            <p>Contact</p>
+    </div>
+    }
+    </>
     )
 
 }
